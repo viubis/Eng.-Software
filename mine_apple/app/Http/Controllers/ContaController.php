190a0e6f1039a70278fd929b137927525c0cc483
Lucas Alves <lucas2213690@gmail.com>
2018-06-05 16:25:32 -0300

    Models e seus respectivos Controllers criados

---
+++ mine_apple/app/Http/Controllers/ContaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Conta;
+use Illuminate\Http\Request;
+
+class ContaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Conta  $conta
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Conta $conta)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Conta  $conta
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Conta $conta)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Conta  $conta
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Conta $conta)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Conta  $conta
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Conta $conta)
+    {
+        //
+    }
+}
---
+++ mine_apple/app/Http/Controllers/ProdutorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Produtor;
+use Illuminate\Http\Request;
+
+class ProdutorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Produtor  $produtor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Produtor $produtor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Produtor  $produtor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Produtor $produtor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Produtor  $produtor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Produtor $produtor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Produtor  $produtor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Produtor $produtor)
+    {
+        //
+    }
+}
