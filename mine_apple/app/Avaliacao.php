015b925c6e029e3a8e36334c6c8eca8a5a02f297
Sesaque Oliveira <sesaqueoliveira@gmail.com>
2018-06-06 07:30:11 -0300

    Classe Avaliacao

--- mine_apple/app/Avaliacao.php
+++ mine_apple/app/Avaliacao.php
@@ -1,15 +1,16 @@
-<?php
-
-namespace App;
+<?php namespace mine_apple;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Avaliacao extends Model
-{
-    protected $tabela = 'avaliacao';
-    protected$campos = [
-    	'idAssinatura',
-        'nota',
-        'comentario'
-    ];
+class Avaliacao extends Model {
+
+    protected $tabel = 'avaliacao';
+    protected $primaryKey = 'idAssinatura';
+    public $timestamps = false
+    protected $guarded = [];
+
+    public function assinatura() {
+    	return $this->hasOne('App/Assinatura', 'foreign_key');
+    }
+
 }
