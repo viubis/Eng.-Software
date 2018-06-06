ff36374d2bb35a27d2043cf9a98d4b110b25268d
Sesaque Oliveira <sesaqueoliveira@gmail.com>
2018-06-06 07:30:34 -0300

    Classe Compra

--- mine_apple/app/Compra.php
+++ mine_apple/app/Compra.php
@@ -1,18 +1,23 @@
-<?php
-
-namespace App;
+<?php namespace mine_apple;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Compra extends Model
-{
-    protected $tabela = 'compra';
-    protected$campos = [
-    	'id',
-        'idConsumidor',
-        'idEndereco',
-        'data',
-        'valor_Total',
-        'valor_Frete'
-    ];
+class Compra extends Model {
+
+    protected $tabel = 'compra';
+    public $timestamps = false
+    protected $guarded = ['id'];
+
+    public function consumidor() {
+    	return $this->hasOne('App/Consumidor', 'foreign_key');
+    }
+
+    public function assinatura() {
+    	return $this->hasMany('App/Assinatura', 'foreign_key');
+    }
+
+    public function endereco() {
+    	return $this->hasOne('App/Endereco', 'foreign_key');
+    }
+
 }
