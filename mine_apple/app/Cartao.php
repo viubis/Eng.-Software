7bc3c08be8cf1d8d60a8716f4004e7646c06a93f
Sesaque Oliveira <sesaqueoliveira@gmail.com>
2018-06-06 07:30:23 -0300

    Classe Cartao

--- mine_apple/app/Cartao.php
+++ mine_apple/app/Cartao.php
@@ -1,18 +1,20 @@
-<?php
-
-namespace App;
+<?php namespace mine_apple;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Cartao extends Model
-{
-    protected $tabela = 'cartao';
-    protected$campos = [
-    	'numero',
-        'titular',
-        'validade',
-        'codigo',
-        'credito',
-        'bandeira'
-    ];
+class Cartao extends Model {
+
+    protected $tabel = 'cartao';
+    protected $primaryKey = 'numero';
+    public $timestamps = false
+    protected $guarded = [];
+
+    public function pagamentos() {
+    	return $this->hasMany('App/Pagamento', 'foreign_key');
+    }
+
+    public function consumidor() {
+    	return $this->hasMany('App/Consumidor', 'cartaoConsumidor', 'idConsumidor', 'cartao_numero');
+    }
+
 }
