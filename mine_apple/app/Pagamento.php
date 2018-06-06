9b27d826f21b2138f32a3a5a3fd3513f3e216c03
Sesaque Oliveira <sesaqueoliveira@gmail.com>
2018-06-06 07:30:50 -0300

    Classe Pagamento

--- mine_apple/app/Pagamento.php
+++ mine_apple/app/Pagamento.php
@@ -1,18 +1,19 @@
-<?php
-
-namespace App;
+<?php namespace mine_apple;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Pagamento extends Model
-{
-    protected $tabela = 'pagamento';
-    protected$campos = [
-    	'id',
-        'idAssinatura',
-        'data',
-        'hora',
-        'cartao_numero',
-        'valor_Pago'
-    ];
+class Pagamento extends Model {
+
+    protected $tabel = 'pagamento';
+    public $timestamps = false
+    protected $guarded = ['id'];
+
+    public function cartao() {
+    	return $this->hasOne('App/Cartao', 'foreign_key');
+    }
+
+    public function assinatura() {
+    	return $this->hasOne('App/Assinatura', 'foreign_key');
+    }
+
 }
