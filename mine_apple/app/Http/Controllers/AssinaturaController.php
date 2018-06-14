ac90074706a1a6fc0edc775377ee274e937b086c
Sesaque Oliveira <sesaqueoliveira@gmail.com>
2018-06-13 23:50:52 -0300

    Revert "Controllers limpos"
    This reverts commit d46e41dcd0d29f82c88afe891de50653878b91b1.

---
+++ mine_apple/app/Http/Controllers/AssinaturaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Assinatura;
+use Illuminate\Http\Request;
+
+class AssinaturaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Assinatura  $assinatura
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Assinatura $assinatura)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Assinatura  $assinatura
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Assinatura $assinatura)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Assinatura  $assinatura
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Assinatura $assinatura)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Assinatura  $assinatura
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Assinatura $assinatura)
+    {
+        //
+    }
+}
