93df6207fe4f002b8ee140e959388ba53d03d7fc
Victória Gomes <38071157+victoriaogomes@users.noreply.github.com>
2018-06-30 22:25:02 -0300

    Terceiro banner do site

--- mine_apple/resources/views/index.blade.php
+++ mine_apple/resources/views/index.blade.php
@@ -221,13 +221,13 @@
         </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img class="d-block w-100" src="{{asset('images/banner_background4.png')}}" alt="First slide">
+                <img class="d-block w-100" src="{{asset('images/banner_background1.png')}}" alt="First slide">
             </div>
             <div class="carousel-item">
                 <img class="d-block w-100" src="{{asset('images/banner_background2.png')}}" alt="Second slide">
             </div>
             <div class="carousel-item">
-                <img class="d-block w-100" src="{{asset('images/banner_background.jpg')}}" alt="Third slide">
+                <img class="d-block w-100" src="{{asset('images/banner_background3.png')}}" alt="Third slide">
             </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
