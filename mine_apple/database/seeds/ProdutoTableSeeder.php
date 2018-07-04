97328ff87823b76890663232d82c2597fa6d8094
Lucas Oliveira <lucas2213690@gmail.com>
2018-07-04 15:24:30 -0700

    Populando mais produtos para teste

--- mine_apple/database/seeds/ProdutoTableSeeder.php
+++ mine_apple/database/seeds/ProdutoTableSeeder.php
@@ -2,6 +2,8 @@
 
 use mine_apple\Produto;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory;
 
 class ProdutoTableSeeder extends Seeder
 {
@@ -12,6 +14,9 @@ class ProdutoTableSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Faker\Factory::create();
+
+
         Produto::create([
         	'produtor_id' => 2,
         	'categoria_id' => 1,
@@ -30,5 +35,27 @@ class ProdutoTableSeeder extends Seeder
         	'sab' => true,
         	'dom' => true
         ]);
+
+        foreach (range(1,20) as $index) {
+            DB::table('produto')->insert([
+                'produtor_id' => 2,
+                'categoria_id' => random_int(1,7),
+                'embalagem_id' => random_int(1,5),
+                'nome' => $faker->name,
+                'descricao' => $faker->name,
+                'valor' => random_int(1,50),
+                'minPorAssinatura' => random_int(1,10),
+                'maxPorDia' => random_int(1,50),
+                'freteMax' => random_int(1,40),
+                'seg' => true,
+                'ter' => true,
+                'qua' => true,
+                'qui' => true,
+                'sex' => false,
+                'sab' => false,
+                'dom' => false
+                
+            ]);
     }
+}
 }
