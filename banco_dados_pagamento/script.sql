c1088d3171beb73069d8041e900bd8d7365285f2
Victória Gomes <38071157+victoriaogomes@users.noreply.github.com>
2018-07-06 22:41:25 -0300

    Adição da criptografia MD5
    Adicionando dados no banco de dados com a criptografia MD5: Está sendo criptografado o número do cartão, o nome do titular e o código de segurança do cartão.

--- banco_dados_pagamento/script.sql
+++ banco_dados_pagamento/script.sql
@@ -9,23 +9,27 @@ USE `moduloPagamento`;
 
 CREATE TABLE IF NOT EXISTS Cartoes(
 	idCartao INT,
-	NumCartao VARCHAR(20),
-    Titular VARCHAR(50),
-    dataVencimento DATE,
-    codSeguranca VARCHAR(4),
+	NumCartao VARCHAR(32),
+    Titular VARCHAR(32),
+    dataVencimento VARCHAR(32),
+    codSeguranca VARCHAR(32),
     statusCartao ENUM('bloqueado', 'vencido', 'ativo'),
-    limiteCredito DOUBLE,
-    limiteUsado DOUBLE,
-    limiteDisponivel DOUBLE,
+    limiteCredito VARCHAR(32),
+    limiteUsado VARCHAR(32),
+    limiteDisponivel VARCHAR(32),
     CONSTRAINT PK_Avaria PRIMARY KEY (idCartao)
 );
 
 INSERT INTO Cartoes VALUES
 -- Cartões de crédito visa
-	("1", "4532133181052174", "Giovana Josefa Ana Novaes", "2019-10-06", "596", "ativo", "23450", "0", "23450"),
-    ("2", "4556271791560706", "Cláudia Cristiane Joana Barros", "2019-02-06", "495", "bloqueado", "4730", "4500", "230"),
+	('1', MD5('4532133181052174'), MD5('Giovana Josefa Ana Novaes'), '2019-10-06', MD5('596'), 'ativo', '23450', '0', '23450'),
+    ('2', MD5('4556271791560706'), MD5('Cláudia Cristiane Joana Barros'), '2019-02-06', MD5('495'), 'bloqueado', '4730', '4500', '230'),
     
 -- Cartões de crédito mastercard
-    ("3", "5446328842585453", "Malu Antonella Rocha", "2020-03-06", "259", "vencido", "4730", "4500", "230");
+    ('3', MD5('5446328842585453'), MD5('Malu Antonella Rocha'), '2020-03-06', MD5('259'), 'vencido', '4730', '4500', '230'),
+    ('4', MD5('5388098949556034'), MD5('Arthur Edson Bernardo Gomes'), '2020-02-07', MD5('793'), 'ativo','2900','500','2400'),
 
 
+-- Cartões de crédito elo
+	('5', MD5('4576644777404014'), MD5('Amanda Vitória Eliane Cardoso'), '2019-09-07', MD5('103'), 'ativo','2000', '500', '1500'),
+    ('6', MD5('6363686303975555'), MD5('Marcelo Diogo Pedro Corte Real'), '2019-03-07', MD5('166'), 'bloqueado', '8000', '7000', '1000');
