92fc0f3a7b12ed60a9b5c52b4e9af261ba0505af
Lucas Oliveira <lucas2213690@gmail.com>
2018-07-16 07:49:48 -0700

    geração de Logs para consumidor e produtor

--- mine_apple/app/Http/Controllers/ConsumidorController.php
+++ mine_apple/app/Http/Controllers/ConsumidorController.php
@@ -109,6 +109,13 @@ class ConsumidorController extends Controller
 
         $cartao->save();
 
+        $log = new Log;
+        $log->usuario_id = Auth::user()->id;
+        $log->operacao_id = 1;
+        $log->data = $data->toDateString();
+        $log->hora = $data->toTimeString();
+        $log->save();
+
         return redirect()->route('consumidor');
 
     }
@@ -123,6 +130,13 @@ class ConsumidorController extends Controller
        $consumidor->fill($request->all()->save());//Salva os dados
 
        /*return redirect()->route('alterar_consumidor', $consumidor->id)->with('info', 'Dados Alterados com sucesso');*/
+
+       $log = new Log;
+       $log->usuario_id = Auth::user()->id;
+       $log->operacao_id = 2;
+       $log->data = $data->toDateString();
+       $log->hora = $data->toTimeString();
+       $log->save();
    }
 
     /**
@@ -338,6 +352,13 @@ class ConsumidorController extends Controller
         $compra->hora = $data->toTimeString();
         $compra->frete = 10;
         $compra->save();
+
+        $log = new Log;
+        $log->usuario_id = Auth::user()->id;
+        $log->operacao_id = 3;
+        $log->data = $data->toDateString();
+        $log->hora = $data->toTimeString();
+        $log->save();
 
     }
 
