7dc33f8bd9f3e363ef959b3666ae8d275c3d0a79
Lucas Oliveira <lucas2213690@gmail.com>
2018-07-18 09:18:03 -0700

    Melhorias no relatorio

--- mine_apple/database/seeds/LogTableSeeder.php
+++ mine_apple/database/seeds/LogTableSeeder.php
@@ -7,7 +7,7 @@ class LogTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
+     * 3 consumidor e 6 para produtor
      * @return void
      */
     public function run()
@@ -28,6 +28,46 @@ class LogTableSeeder extends Seeder
     		'operacao_id' => 1
 
     	]);
+
+        Log::create([
+            'usuario_id' => 1,
+            'data' => '2018-01-10',
+            'hora' => '09:50:00',
+            'operacao_id' => 3
+
+        ]);
+
+        Log::create([
+            'usuario_id' => 1,
+            'data' => '2018-01-10',
+            'hora' => '06:30:19',
+            'operacao_id' => 3
+
+        ]);
+
+        Log::create([
+            'usuario_id' => 2,
+            'data' => '2018-06-02',
+            'hora' => '16:30:19',
+            'operacao_id' => 6
+
+        ]);
+
+        Log::create([
+            'usuario_id' => 2,
+            'data' => '2018-06-09',
+            'hora' => '22:30:19',
+            'operacao_id' => 6
+
+        ]);
+
+        Log::create([
+            'usuario_id' => 2,
+            'data' => '2018-03-10',
+            'hora' => '18:30:19',
+            'operacao_id' => 6
+
+        ]);
 
     }
 }
