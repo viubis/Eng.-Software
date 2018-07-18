63b51a2b0f3cee81cdbb97e936fec25eee150383
VanessaSOliveira <vanessasouzaoliveira1@gmail.com>
2018-07-18 12:27:52 -0300

    asset

--- mine_apple/resources/views/alterar_dados_consumidor.blade.php
+++ mine_apple/resources/views/alterar_dados_consumidor.blade.php
@@ -10,11 +10,10 @@
     <title>Alterar dados de consumidor</title>
 
     <!-- CSS -->
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+    <link rel="stylesheet" href="{{asset('http://fonts.googleapis.com/css?family=Roboto:400,100,300,500')}}">
     <link rel="stylesheet" href="{{asset('css/bootstrap4/bootstrap.min.css')}}">
     <link href="{{asset('plugins/fontawesome-free-5.0.1/css/fontawesome-all.css')}}" rel="stylesheet" type="text/css">
-    <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css" rel="stylesheet"
-          type='text/css'>
+    <link href="{{asset('http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css')}}" rel="stylesheet" type='text/css'>
     <link rel="stylesheet" href="{{asset('css/formularios_style.css')}}">
 
     <!-- Favicon and touch icons -->
